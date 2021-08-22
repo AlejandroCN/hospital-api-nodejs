041fb173817888cdcea418206b9cb191259e668f
Alejandro Cruz Noguez <alecruznz@gmail.com>
2021-08-22 14:22:46 -0500

    Configuración de BD agregada al .gitignore

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports.secret_key = 'dRt47r664';
-module.exports.database = 'hospital';
-module.exports.user = 'admin';
-module.exports.password = 'Pkmn05Blue';
-module.exports.host = 'localhost';
-module.exports.dialect = 'mysql';
